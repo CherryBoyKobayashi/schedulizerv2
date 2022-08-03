9610bf03f2db0c33514db7f68e15f9f2e81bfa9f
YukiT97 <90177303+YukiT97@users.noreply.github.com>
2022-08-03 16:18:42 +0900

    m0803

--- server/index.js
+++ server/index.js
@@ -10,26 +10,31 @@ app.post('/userDB', async (req, res) => {
   await db.read()
   db.data ||= { users: [] }
   const {users} = db.data
-  let user = users.find((u) => u.username == req.body.username)
-  if(user != undefined) {
-    if(user.password == req.body.password) {
-      res.send(user);
+  if (req.body.username != undefined) {
+    let user = users.find((u) => u.username == req.body.username)
+    if(user != undefined) {
+      if(user.password == req.body.password) {
+        res.send(user);
+      }
+      else {
+        res.sendStatus(401);
+      }
     }
     else {
-      res.sendStatus(401);
+      if (req.body.mail == undefined) {
+        res.send(204);
+      } else {
+        const {users} = db.data
+        users.push(req.body)
+        const {user_projects} = db.data
+        user_projects.push({username: req.body.username, projects: []})
+        await db.write()
+        res.sendStatus(200)
+      }
     }
   }
   else {
-    if (req.body.mail == undefined) {
-      res.send(204);
-    } else {
-      const {users} = db.data
-      users.push(req.body)
-      const {user_projects} = db.data
-      user_projects.push({username: req.body.username, projects: []})
-      await db.write()
-      res.sendStatus(200)
-    }
+    res.send(users.map(a => a.username))
   }
 })
 
@@ -115,6 +120,80 @@ app.post('/projectDBdelete', async (req, res) => {
     res.sendStatus(200);
   }
   else {
+    res.send(204);
+  }
+})
+
+app.post('/milestoneDB', async (req, res) => {
+  await db.read()
+  db.data ||= { projects: [] }
+  const {projects} = db.data
+  let project = projects.find((u) => u.projectId == req.body.projectId)
+  if(project != undefined) {
+    if(req.body.projectData != undefined) {
+      project.projectData = req.body.projectData
+      await db.write()
+      res.sendStatus(200)
+    } else {
+      if(project.projectData != undefined) {
+        if (project.projectData.filter(data => data.milestoneId == req.body.milestoneId).length != 0) {
+          if (req.body.milestoneName != undefined){
+            delete req.body.projectId;
+            let milestone = project.projectData.filter(data => data.milestoneId == req.body.milestoneId)[0]
+            milestone.milestoneName = req.body.milestoneName;
+            milestone.color = req.body.color;
+            milestone.tasks = req.body.tasks;
+            await db.write()
+            res.sendStatus(200)
+          } else {
+            project.projectData = project.projectData.filter(data => data.milestoneId != req.body.milestoneId)
+            await db.write()
+            res.sendStatus(200)
+          }
+        } else {
+          delete req.body.projectId;
+          project.projectData.push(req.body)
+          await db.write()
+          res.sendStatus(200)
+        }
+      }
+    }
+  }
+  else {
+    if (req.body.userId == undefined) {
+      res.send(204);
+    }
+  }
+})
+
+app.post('/taskDB', async (req, res) => {
+  await db.read()
+  try {
+    db.data ||= { projects: [] }
+    const {projects} = db.data
+    let project = projects.find((u) => u.projectId == req.body.projectId)
+    let milestone = project.projectData.filter(data => data.milestoneId == req.body.milestoneId)[0]
+    let t = milestone.tasks.filter(data => data == req.body.taskId)
+    let {tasks} = db.data
+    if (t.length == 0) {
+      milestone.tasks.push(req.body.taskId);
+      delete req.body.projectId;
+      delete req.body.milestoneId;
+      tasks.push(req.body);
+      await db.write();
+    }
+    else {
+      delete req.body.projectId;
+      delete req.body.milestoneId;
+      db.data.tasks = db.data.tasks.map(obj => [req.body].find(o => o.taskId == obj.taskId) || obj)
+      // (obj => arr2.find(o => o.id === obj.id) || obj)
+      // tasks.find(task => task.taskId == req.body.taskId)
+      console.log(tasks);
+      await db.write();
+    }
+    res.sendStatus(200);
+  }
+  catch {
     res.send(204);
   }
 })
